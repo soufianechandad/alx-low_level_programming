0e11ec64bdba5e0edc024e593b2123da5f92fd56
soufianechandad <soufiane.chandad@gmail.com>
2023-06-16 02:02:08 +0200

    hard

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/**
+/* *
  * * main - Entry point
  * *
  * * Return: Always 0 (Success)
@@ -9,4 +9,3 @@ int main(void)
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
-
