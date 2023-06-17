e2bf14d379663ce9d5eb3e318d93975fb5520430
soufianechandad <soufiane.chandad@gmail.com>
2023-06-17 02:12:21 +0200

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 52; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
