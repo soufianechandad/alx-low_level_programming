fe2a686b8fa462e2b71f784b17109ff2a3daa30d
soufianechandad <soufiane.chandad@gmail.com>
2023-06-30 11:28:37 -0700

    sw

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,11 +3,11 @@
 int main(void)
 {
 int n;
-int t[5];
+int a[5];
 int *p;
 
-t[2] = 1024;
-l = &n;
+a[2] = 1024;
+p = &n;
 /*
  * write your line of code here...
  * Remember:
@@ -18,6 +18,6 @@ l = &n;
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
-printf("t[2] = %d\n", t[2]);
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
