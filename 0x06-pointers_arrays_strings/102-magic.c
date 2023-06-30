43c228c4c43d0a2f43954f50a594a80ff1e32c4e
soufianechandad <soufiane.chandad@gmail.com>
2023-06-30 11:25:13 -0700

    sd

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,22 +2,22 @@
 
 int main(void)
 {
-  int n;
-  int b[5];
-  int *l;
+int n;
+int t[5];
+int *p;
 
-  b[2] = 1024;
-  l = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(l + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("b[2] = %d\n", b[2]);
-  return (0);
+t[2] = 1024;
+l = &n;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("t[2] = %d\n", t[2]);
+return (0);
 }
