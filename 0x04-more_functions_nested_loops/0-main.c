f024e5b20908dfc306b731a67877c2eeb0732e92
soufianechandad <soufiane.chandad@gmail.com>
2023-07-01 03:33:19 -0700

    SW

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -5,14 +5,14 @@
  * main - check the code.
  *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
-char c;
+    char c;
 
-c = 'A';
-printf("%c: %d\n", c, _isupper(c));
-c = 'a';
-printf("%c: %d\n", c, _isupper(c));
-return (0);
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
