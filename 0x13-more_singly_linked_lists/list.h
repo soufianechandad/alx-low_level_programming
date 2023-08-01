53a3b7417297654fc0109a8a1346a1b0602fcf70
soufianechandad <soufiane.chandad@gmail.com>
2023-08-01 16:28:55 -0700

    nex

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -12,13 +12,12 @@ lifndef LISTS_H
  *
  * Description: singly linked list node structure
  * for alx project
- */
+*/
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -36,4 +35,3 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
-
