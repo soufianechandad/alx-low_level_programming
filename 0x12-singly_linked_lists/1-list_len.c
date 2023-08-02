0d48dc39fed06653daa0b69822867a0587b80b20
soufianechandad <soufiane.chandad@gmail.com>
2023-08-02 10:27:08 -0700

    135

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,16 +6,15 @@
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
- */
+*/
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+while (h)
+{
+n++;
+h = h->next;
 }
-
+return (n);
+}
