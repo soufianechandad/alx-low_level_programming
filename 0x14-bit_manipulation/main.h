af9dc38858a33509c7eecabe69ba928528fe402a
soufianechandad <soufiane.chandad@gmail.com>
2023-08-04 06:31:45 -0700

    fer

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -11,4 +11,4 @@ int _atoi(const char *s);
 int _putchar(char c);
 int get_endianness(void);
 
-#end
+#endif
