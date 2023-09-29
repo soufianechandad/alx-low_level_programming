e21853fae95f6121cedc2c44559d45c7a0ef3e24
soufianechandad <soufiane.chandad@gmail.com>
2023-09-29 21:35:21 +0100

    FER

--- 0x14-bit_manipulation/0-BINARY_TO_UINT.C
+++ 0x14-bit_manipulation/0-BINARY_TO_UINT.C
@@ -2,25 +2,25 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
+ * @binary_string: string containing the binary number
  *
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *binary_string)
 {
-	int i;
-	unsigned int dec_val = 0;
+    int i;
+    unsigned int decimal_value = 0;
 
-	if (!b)
-		return (0);
+    if (!binary_string)
+        return (0);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
+    for (i = 0; binary_string[i]; i++)
+    {
+        if (binary_string[i] < '0' || binary_string[i] > '1')
+            return (0);
+        decimal_value = 2 * decimal_value + (binary_string[i] - '0');
+    }
 
-	return (dec_val);
+    return (decimal_value);
 }
 
