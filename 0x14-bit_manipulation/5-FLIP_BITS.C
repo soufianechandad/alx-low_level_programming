79d4cf479f9123ec58d907112682a79c63557292
soufianechandad <soufiane.chandad@gmail.com>
2023-09-29 12:32:21 +0100

    fer

--- 0x14-bit_manipulation/5-FLIP_BITS.C
+++ 0x14-bit_manipulation/5-FLIP_BITS.C
@@ -7,20 +7,21 @@
  * @m: second number
  *
  * Return: number of bits to change
-*/
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-int a, countbit = 0;
-unsigned long int current;
-unsigned long int exclusive = n ^ m;
+	int a, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-for (a = 63; a >= 0; a--)
-{
-current = exclusive >> a;
-if (current & 1)
-countbit++;
+	for (a = 63; a >= 0; a--)
+	{
+		current = exclusive >> a;
+		if (current & 1)
+			countbit++;
+	}
+
+	return (countbit);
 }
 
-return (countbit);
-}
 
