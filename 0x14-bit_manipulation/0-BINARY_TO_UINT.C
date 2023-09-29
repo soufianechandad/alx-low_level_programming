6c61a7da6edb403e3b8a8697159c862e5284fc3b
soufianechandad <soufiane.chandad@gmail.com>
2023-09-29 21:40:46 +0100

    D

--- 0x14-bit_manipulation/0-BINARY_TO_UINT.C
+++ 0x14-bit_manipulation/0-BINARY_TO_UINT.C
@@ -5,22 +5,21 @@
  * @binary_string: string containing the binary number
  *
  * Return: the converted number
- */
+*/
 unsigned int binary_to_uint(const char *binary_string)
 {
-    int i;
-    unsigned int decimal_value = 0;
+int i;
+unsigned int decimal_value = 0;
 
-    if (!binary_string)
-        return (0);
+if (!binary_string)
+return (0);
 
-    for (i = 0; binary_string[i]; i++)
-    {
-        if (binary_string[i] < '0' || binary_string[i] > '1')
-            return (0);
-        decimal_value = 2 * decimal_value + (binary_string[i] - '0');
-    }
-
-    return (decimal_value);
+for (i = 0; binary_string[i]; i++)
+{
+if (binary_string[i] < '0' || binary_string[i] > '1')
+return (0);
+decimal_value = 2 * decimal_value + (binary_string[i] - '0');
 }
 
+return (decimal_value);
+}
